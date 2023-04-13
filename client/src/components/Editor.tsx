eb5d707747928d5fade60f9394626a486435e4e6
ArjunCybro <78548960+arjunkdot@users.noreply.github.com>
2023-04-13 21:52:54 +0530

    :construction: Markdown conversion added.

--- client/src/components/Editor.tsx
+++ client/src/components/Editor.tsx
@@ -1,6 +1,14 @@
+import React, { useEffect, useState } from "react";
 import Button from "./Button";
+import ReactMarkdown from "react-markdown";
 
 const Editor = () => {
+  const [text, setText] = useState("");
+
+  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setText(e.target.value);
+  };
+
   return (
     <div className="flex items-stretch h-[calc(100vh_-_72px)]">
       <div className="w-1/2 h-full">
@@ -9,7 +17,10 @@ const Editor = () => {
             Markdown
           </span>
         </div>
-        <textarea className='w-full h-full resize-none focus:outline-none p-5 border-r border-r-custom-grey-100 font-roboto-mono font-light text-custom-text-heading-sm leading-[24px]'>
+        <textarea
+          onChange={handleChange}
+          className="w-full h-full resize-none focus:outline-none p-5 border-r border-r-custom-grey-100 font-roboto-mono font-light text-custom-text-heading-sm leading-[24px]"
+        >
           # Welcome to Markdown
         </textarea>
       </div>
@@ -20,6 +31,9 @@ const Editor = () => {
             Preview
           </span>
           <Button mode="transparent" icon="show"></Button>
+        </div>
+        <div className="w-full h-full p-5">
+          <ReactMarkdown>{text}</ReactMarkdown>
         </div>
       </div>
     </div>
