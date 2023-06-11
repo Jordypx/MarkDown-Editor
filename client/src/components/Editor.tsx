f989355edfecda3b25f215e7e21c5a46f29dcaf8
ArjunCybro <78548960+arjunkdot@users.noreply.github.com>
2023-06-11 15:03:38 +0530

    :construction: Full width preview switcher added.

--- client/src/components/Editor.tsx
+++ client/src/components/Editor.tsx
@@ -8,7 +8,7 @@ interface EditorProps
 }
 const Editor = (props: EditorProps) => {
   const [text, setText] = useState(props.value);
-
+  const [isFullWidthPrevieOn, setIsFullWidthPrevieOn] = useState(false);
   useEffect(() => {
     setText(props.value);
   }, [props.value]);
@@ -19,33 +19,37 @@ const Editor = (props: EditorProps) => {
 
   return (
     <div className="flex items-stretch h-[calc(100vh_-_72px)]">
-      <div className="w-1/2 h-full">
-        <div className="h-[42px] bg-custom-white-200 flex items-center border-r border-r-custom-grey-100 px-5">
-          <span className="text-custom-text-heading-sm uppercase tracking-[2px] text-custom-grey-300">
-            Markdown
-          </span>
+      {!isFullWidthPrevieOn && (
+        <div className="w-1/2 h-full">
+          <div className="h-[42px] bg-custom-white-200 flex items-center border-r border-r-custom-grey-100 px-5">
+            <span className="text-custom-text-heading-sm uppercase tracking-[2px] text-custom-grey-300">
+              Markdown
+            </span>
+          </div>
+          <textarea
+            onChange={handleChange}
+            className="w-full h-full resize-none focus:outline-none px-5 py-3 border-r border-r-custom-grey-100 font-roboto-mono font-light text-custom-text-heading-sm leading-[24px]"
+            {...props}
+            value={text}
+          ></textarea>
         </div>
-        <textarea
-          onChange={handleChange}
-          className="w-full h-full resize-none focus:outline-none px-5 py-3 border-r border-r-custom-grey-100 font-roboto-mono font-light text-custom-text-heading-sm leading-[24px]"
-          {...props}
-          value={text}
-        ></textarea>
-      </div>
-
-      <div className="w-1/2 h-full">
+      )}
+      <div className={`${isFullWidthPrevieOn ? "w-full" : "w-1/2"} h-full`}>
         <div className="h-[42px] bg-custom-white-200 flex items-center justify-between px-5">
           <span className="text-custom-text-heading-sm uppercase tracking-[2px] text-custom-grey-300">
             Preview
           </span>
           <Button
+            onClick={() => setIsFullWidthPrevieOn(!isFullWidthPrevieOn)}
             mode="transparent"
-            icon="show"
+            icon={isFullWidthPrevieOn ? "hide" : "show"}
             aria-label="Hide Editor"
           ></Button>
         </div>
         <div className="w-full h-full p-5 overflow-y-auto md-rendered">
-          <ReactMarkdown>{text}</ReactMarkdown>
+          <div className={`${isFullWidthPrevieOn ? "w-1/2 mx-auto" : "w-full"} h-full`}>
+            <ReactMarkdown>{text}</ReactMarkdown>
+          </div>
         </div>
       </div>
     </div>
